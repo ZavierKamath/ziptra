5d2a5cb504a0e762d47986e0061c80e7cc631d82
ZavierKamath <zavierkamath@gmail.com>
2026-08-05 23:26:46 -0400

    Added db row interfaces to represent the relational database row models. These will be different from the object models that are passed between frontend and backend

--- backend/src/models.ts
+++ backend/src/models.ts
@@ -1,19 +1,44 @@
 export type TaskStatus = "New" | "Explore" | "Build" | "Validate" | "Closed"
 export type ProjectStatus = "New" | "Active" | "Closed"
+export type ParentType = "Task" | "Project"
 
 export type Comment = {
 	commentId: string,
+	parentType: ParentType,
+	parentId: string,
 	createdAt: Date,
+	updatedAt: Date,
+	content: string
+}
+
+export interface CommentRow {
+	commentId: string,
+	parentType: string,
+	parentId: string,
+	createdAt: string,
+	updatedAt: string,
 	content: string
 }
 
 export type Task = {
 	taskId: string,
+	projectId: string,
 	title: string,
 	description: string,
 	comments: Comment[],
 	createdAt: Date,
+	updatedAt: Date,
 	status: TaskStatus
+}
+
+export interface TaskRow {
+	taskId: string,
+	projectId: string,
+	title: string,
+	description: string,
+	createdAt: string,
+	updatedAt: string,
+	status: string
 }
 
 export type Project = {
@@ -22,5 +47,15 @@ export type Project = {
 	description: string,
 	comments: Comment[],
 	createdAt: Date,
+	updatedAt: Date,
 	status: ProjectStatus
+}
+
+export interface ProjectRow {
+	projectId: string,
+	title: string,
+	description: string,
+	createdAt: string,
+	updatedAt: string,
+	status: string
 }
